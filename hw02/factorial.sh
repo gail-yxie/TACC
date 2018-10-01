575cbcb9d313dc73a1e66901de3134e86d66453f
Gail <yuege@ices.utexas.edu>
2018-10-01 18:06:27 -0500

    YX: fix bugs for n=0 and n is negative

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -7,11 +7,19 @@ read -p "Pleas input n:" n
 # initialize
 fact=1
 
-# use for loop to calculate
-for (( i=1;i<=n;i++))
-	do
-		fact=$(expr $fact \* $i) 
-	done
+if [ $n -ge 1 ]
+	then
+		# use for loop to calculate
+		for (( i=1;i<=n;i++))
+			do
+				fact=$(expr $fact \* $i) 
+			done
+elif [ $n -lt 0 ]
+	then
+		fact="Please input a right number."
+else
+	fact=1
+fi
 
 # output
 echo $fact
