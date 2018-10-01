543e6eba25e512d50d6c60dcaeee7d7998400948
Gail <yuege@ices.utexas.edu>
2018-10-01 17:36:52 -0500

    YX: add comment lines to factorial.sh

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # It will calculate the factorial of a supplied number.
 
+# input the supplied number
 read -p "Pleas input n:" n
+
+# initialize
 fact=1
 
+# use for loop to calculate
 for (( i=1;i<=n;i++))
 	do
 		fact=$(expr $fact \* $i) 
 	done
 
+# output
 echo $fact
