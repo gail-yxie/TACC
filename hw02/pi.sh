898e2792cae9f769df02aa3c323139e1fadd7336
Gail <yuege@ices.utexas.edu>
2018-10-01 22:14:06 -0500

    YX: add for loop to pi.sh to iterate and count

--- hw02/pi.sh
+++ hw02/pi.sh
@@ -2,8 +2,29 @@
 # It will estimate the value of pi using sampling
 
 # define RandomDraw
-function RandomDraw(){
+RandomDraw(){
 	echo "scale=15; $RANDOM/32767" | bc -l
 }
 
-RandomDraw
+N_samples=$1
+r=$(echo "scale=15; 1/1" | bc -l)
+N_i=0
+N_o=0
+
+# use for loop to count
+for (( i=1;i<=$N_samples;i=i+1 ))
+        do
+                x=$(RandomDraw)
+                y=$(RandomDraw)
+		r2=$(echo "$x * $x + $y * $y" | bc -l)
+                if [ $(echo "$r2 > $r" | bc) -eq 1 ];then
+                                N_o=$(( N_o + 1 ))
+                fi
+        done
+
+N_i=$(( $N_samples -  $N_o ))
+
+echo "$N_i"
+
+
+
