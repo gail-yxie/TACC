dd62f789e9c958316ce90e23f907f81789bff175
Gail <yuege@ices.utexas.edu>
2018-10-01 22:59:36 -0500

    YX: add the function to calculate relative error

--- hw02/pi.sh
+++ hw02/pi.sh
@@ -16,15 +16,19 @@ for (( i=1;i<=$N_samples;i=i+1 ))
         do
                 x=$(RandomDraw)
                 y=$(RandomDraw)
-		r2=$(echo "$x * $x + $y * $y" | bc -l)
+		r2=$(echo "scale=15; $x * $x + $y * $y" | bc -l)
                 if [ $(echo "$r2 > $r" | bc) -eq 1 ];then
                                 N_o=$(( N_o + 1 ))
                 fi
         done
 
+# calculate parameters
 N_i=$(( $N_samples -  $N_o ))
+pi_estimate=$(echo "scale=15; $N_i/$N_samples*4" | bc -l)
+e_rel=$(echo "scale=4; $pi_estimate/3.14159265-1" | bc -l)
 
-echo "$N_i"
+# output
+echo $N_samples $N_i $N_o $pi_estimate $e_rel
 
 
 
