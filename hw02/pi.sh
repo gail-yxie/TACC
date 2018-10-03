aad5b912d8e5ba443b5e4899e44735c55819287e
Yuege Xie <yx1023@login3.stampede2.tacc.utexas.edu>
2018-10-03 18:59:57 -0500

    YX: add the check for #arguments and adjust the format

--- hw02/pi.sh
+++ hw02/pi.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 # It will estimate the value of pi using sampling
+# Author: Yuege Xie
 
-# define RandomDraw
-RandomDraw(){
-	echo "scale=15; $RANDOM/32767" | bc -l
-}
+# Check the number of arguments
 
-N_samples=$1
-r=$(echo "scale=15; 1/1" | bc -l)
-N_i=0
-N_o=0
+if [ $# -lt 1 ];then
+	echo "Error! Please input at least one number for N_samples/"
+	exit
+else
 
-# use for loop to count
-for (( i=1;i<=$N_samples;i=i+1 ))
-        do
-                x=$(RandomDraw)
-                y=$(RandomDraw)
-		r2=$(echo "scale=15; $x * $x + $y * $y" | bc -l)
-                if [ $(echo "$r2 > $r" | bc) -eq 1 ];then
-                                N_o=$(( N_o + 1 ))
-                fi
-        done
+	# define RandomDraw
+	RandomDraw(){
+		echo "scale=15; $RANDOM/32767" | bc -l
+	}
 
-# calculate parameters
-N_i=$(( $N_samples -  $N_o ))
-pi_estimate=$(echo "scale=15; $N_i/$N_samples*4" | bc -l)
-e_rel=$(echo "scale=4; $pi_estimate/3.141592653589793-1" | bc -l)
+	N_samples=$1
+	r=$(echo "scale=15; 1/1" | bc -l)
+	N_i=0
+	N_o=0
 
-# get absolute value of relative error
-if [ $(echo "$e_rel < 0" | bc) -eq 1 ];then
-        e_rel=$(echo "scale=4; 0 - $e_rel" | bc -l)
+	# use for loop to count
+	for (( i=1;i<=$N_samples;i=i+1 ))
+        	do
+                	x=$(RandomDraw)
+                	y=$(RandomDraw)
+			r2=$(echo "scale=15; $x * $x + $y * $y" | bc -l)
+                	
+			if [ $(echo "$r2 > $r" | bc) -eq 1 ];then
+                        	N_o=$(( N_o + 1 ))
+			fi
+		done
+
+
+	# calculate parameters
+	N_i=$(( $N_samples -  $N_o ))
+	pi_estimate=$(echo "scale=15; $N_i/$N_samples*4" | bc -l)
+	e_rel=$(echo "scale=4; $pi_estimate/3.141592653589793-1" | bc -l)
+
+	# get absolute value of relative error
+	if [ $(echo "$e_rel < 0" | bc) -eq 1 ];then
+        	e_rel=$(echo "scale=4; 0 - $e_rel" | bc -l)
+	fi
+
+	# output
+	echo $N_samples $N_i $N_o $pi_estimate $e_rel
 fi
-
-# output
-echo $N_samples $N_i $N_o $pi_estimate $e_rel
-
-
 
