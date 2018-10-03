b6ce893879552bdd8e90235b7440a938e3f47838
Yuege Xie <yx1023@login4.stampede2.tacc.utexas.edu>
2018-10-03 18:26:50 -0500

    YX: use a for loop to generate two triangles

--- hw02/tri.sh
+++ hw02/tri.sh
@@ -2,8 +2,22 @@
 # It will generate triangles for numbers and dots
 # Author: Yuege Xie
 
-for (( i=1;i<=5;i=i+1 ))
+for (( i=1;i<=10;i=i+1 ))
 	do
-		printf $i
+ 
+	# print the first triangle
+	if [ $i -le 5 ]; then
+		res=$(( 5 - $i ))
+		printf " %.0s" $(seq 0 $res)
+		printf "$i %.0s" $(seq 1 $i)
+	        printf "\n"
+	else
+	# print the second triangle
+		res=$(( 10 - $i ))
+		num=$(( $i - 5 ))
+                printf ' %.0s' $(seq 0 $res)
+                printf '. %.0s' $(seq 1 $num)
+		printf "\n"
+	fi
 	done
-
+	
