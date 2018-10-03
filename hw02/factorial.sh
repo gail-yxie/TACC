d1276018d59d10bba2bff4f266697ba7f533edb9
Yuege Xie <yx1023@login1.stampede2.tacc.utexas.edu>
2018-10-03 17:49:44 -0500

    YX: change factorial.sh to while loop and add the checks for the number of inputs and make sure it is nonnegative.

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # It will calculate the factorial of a supplied number.
 
-# input the supplied number
-read -p "Pleas input a nonnegative integer:" n
-
-# initialize
-fact=1
-
-if [ $n -ge 1 ]
-	then
-		# use for loop to calculate
-		for (( i=1;i<=n;i++))
-			do
-				fact=$(expr $fact \* $i) 
-			done
-elif [ $n -lt 0 ]
-	then
-		fact="Please input a right number."
-else
+# check the number of arguments
+if [ $# -lt 1 ];then
+	echo "Error! Please input at least one number to get its factorial."
+	exit
+elif [ $1 -lt 0 ];then
+	echo "Error! Please input a nonnegative number."
+	exit
+else 
+	# initialize
 	fact=1
+	i=$1
+	
+	# do while loop to get result
+	while [ $i -gt 1 ]
+		do
+			fact=$(expr $fact \* $i)
+			i=$(expr $i - 1) 
+		done
+	# output
+	echo $fact
 fi
 
-# output
-echo $fact
