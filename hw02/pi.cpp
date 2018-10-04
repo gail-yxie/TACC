a92e05c1596030afa7f35d6d8fc496ed8bde81e2
Yuege Xie <yx1023@login4.stampede2.tacc.utexas.edu>
2018-10-03 20:16:37 -0500

    YX: modity pi.cpp to get high resolution time

--- hw02/pi.cpp
+++ hw02/pi.cpp
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
+    using namespace std::chrono;
+    high_resolution_clock::time_point start = high_resolution_clock::now();
     if (argc <2) {
         printf ("Error! Please input at least one number!\n");
         exit(0);
@@ -17,7 +20,7 @@ int main(int argc, char *argv[])
     int i, k;
     int N_o, N_i;
     double x, y, dist, pi_estimate, e_rel;
-    double pi, dur;
+    double pi;
     int N_samples = stoi(argv[1]);
 
     srand(time(0));
@@ -43,7 +46,9 @@ int main(int argc, char *argv[])
     if (e_rel < 0)
         e_rel = - e_rel;
 
-    printf ("%d %d %d %0.15f %0.4f ", N_samples, N_i, N_o, pi_estimate, e_rel);
+    high_resolution_clock::time_point end = high_resolution_clock::now();
+    duration<double> dur = duration_cast<duration<double>>(end - start);
+    printf ("%d %d %d %0.15f %0.4f %f\n", N_samples, N_i, N_o, pi_estimate, e_rel, dur);
 
     return 0;
 }
--- hw02/pibatch2.sh
+++ hw02/pibatch2.sh
@@ -3,19 +3,14 @@
 
 # set the number of nodes
 #SBATCH -N 1
-
 # set the number of cores
-#SBATCH -n 32
-
+#SBATCH -n 1
 # set output file name
 #SBATCH -o pi.compiled.log
-
 # set job name
 #SBATCH -J pibatch2
-
 # set submission queue
 #SBATCH -p skx-dev
-
 # set Project name
 #SBATCH -A cse38018
 
@@ -29,5 +24,5 @@ g++ -std=c++11 -o pi pi.cpp
 # run for different n
 for n in 10 100 500 1000 5000 10000 50000
         do
-                /bin/time -f '%e' ./pi $n
+                ./pi $n
         done
