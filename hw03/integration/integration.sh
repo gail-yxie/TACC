966e8345ce83010b96b7d16d71642385109baaeb
Gail <yuege@ices.utexas.edu>
2018-10-22 14:17:15 -0500

    YX: modify the functions and add job script

---
+++ hw03/integration/integration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Yuege Xie
+
+# set the number of nodes
+#SBATCH -N 1
+# set the number of cores
+#SBATCH -n 1 
+# set output file name
+#SBATCH -o integration.log
+# set job name
+#SBATCH -J integration
+# set submission queue
+#SBATCH -p skx-dev
+# set Project name
+#SBATCH -A cse38018
+# set max run time
+#SBATCH -t 00:20:00
+
+
+# run for different methods for different N
+for i in 1 2
+	do
+		for N in 10 20 40 80 160 320 640 1280 2560 5120 10240 20480 
+			do
+	 			./integrate $i $N
+			done
+	done
--- hw03/integration/main.cpp
+++ hw03/integration/main.cpp
@@ -31,6 +31,6 @@ int main(int argc, char *argv[])
 		}
 	else
 		printf("Error, please input method 1 or 2!\n");
-	printf("%d %0.11f %0.11f\n", N, inte, error);
+	printf("%d %d %0.11f %0.11f\n", method, N, inte, error);
 	return 0;
 }
