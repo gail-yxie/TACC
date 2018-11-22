0c25fbdbef80025854bc4ac04a47da5ed382a734
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-22 14:13:21 -0600

    Update solve_system.c

--- proj01/src/solve_system.c
+++ proj01/src/solve_system.c
@@ -3,10 +3,11 @@
 #include "matrix.h"
 #include "error_norm.h"
 
-void solve_system()
+void solve_system(int n, char* iter_method)
 {
 	/* Variable needed: n, iter_method */
 	int i, j, k;
+	double diag;
 	double *dz, *z;
 	dz = (double*) mcalloc(n*sizeof(double));
 	z  = (double*) mcalloc(n*sizeof(double));
@@ -23,8 +24,10 @@ void solve_system()
 				{
 					if(A.col[i][j]!=i)
 						dz[i] -= A.val[i][j] * x[A.col[i][j]];
+					else
+						diag   = A.val[i][j];
 				}
-				dz[i] = dz[i] / A.val[i][]; 
+				dz[i] = dz[i] / diag; 
 			}	
 		
 			if(error_norm(*z,*dz, n)<= eps) 
@@ -33,7 +36,6 @@ void solve_system()
 				for(i=0; i<n; i++)
 					z[i] = dz[i];
 		}
-
 		/*??? Free memory*/	
 		free(z);
 		free(dz);
@@ -41,7 +43,32 @@ void solve_system()
 
 	/* Solve linear system using Gauss-Seidel iteration */
 	if(iter_method == 'Gauss_Seidel')
-	
-
+	{
+		for(k=1; k<max_iter; k++)
+		{
+			for(i=0; i<n; i++)
+			{
+				dz[i] = b[i];
+				for(j=0; j<A.nonzero[i]; j++)
+				{
+					if(A.col[i][j]<i)
+						dz[i] -= A.val[i][j] * dx[A.col[i][j]];
+					else if(A.col[i][j]>i)
+						dz[i] -= A.val[i][j] * x[A.col[i][j]];
+					else
+						diag   = A.val[i][j];	
+				}
+				dz[i] = dz[i] / diag; 
+			}	
+		
+			if(error_norm(*z,*dz, n)<= eps) 
+				break;
+			else  //how to assign value?
+				for(i=0; i<n; i++)
+					z[i] = dz[i];
+		}
+		/*??? Free memory*/	
+		free(z);
+		free(dz);
+	}
 }
-
