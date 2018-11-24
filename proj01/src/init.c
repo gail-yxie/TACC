375c30a339b675c24d2d4763fa9b4d34b0f4687a
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-24 17:56:27 -0600

    Update init.c

--- proj01/src/init.c
+++ proj01/src/init.c
@@ -46,38 +46,46 @@ void init(Parameter* solver)
 
 	if(dimensions == 2)
 	{	
-		masa_init("","");
-		masa_set_param("k",*k);
+		double x,y;
 
-		double* b;
-		b = (double*) malloc((N+1)*(N+1)*sizeof(double));
+		/* Initialize masa: we can choose "bob" or "nick" */
+		/* we can use  "masa_list_mms();" to initialize mms */
+		masa_init("bob","heateq_2d_steady_const");
+
+		/* Set parameter for masa example */
+		// ??? do we use k_0?
+		masa_set_param(k_0, solver.k);
+
+        	solver.b = (double*)malloc((N+1)*(N+1)*sizeof(double));
+		solver.f = (double*)malloc((N+1)*(N+1)*sizeof(double));
+		solver.u = (double*)malloc((N+1)*(N+1)*sizeof(double));
 
 		for(int i=0; i<=N; i++)
 			for(int j=0; j<=N; j++)
 			{	
 				/* generate mesh */
-				x = xmin + i * h;
-				y = ymin + j * h;
+				x = solver.xmin + i * solver.h;
+				y = solver.ymin + j * solver.h;
 
 				/* source term */
-				f[i][j] = masa_eval_2d_source_f(x,y);
+				solver.f[i*(N+1)+j] = masa_eval_2d_source_t(x,y);
 				/* manufactured solution */
-				u[i][j] = masa_eval_2d_exact_phi(x,y);
+				solver.u[i*(N+1)+j] = masa_eval_2d_exact_t(x,y);
 
 				if((i==0)||(i==N)||(j==0)||(j==N))
-					b[i*(N+1)+j] = u[i][j];
+					b[i*(N+1)+j] = u[i*(N+1)+j];
 				else
-					b[i*(N+1)+j] = f[i][j];
+					b[i*(N+1)+j] = f[i*(N+1)+j];
 			}
 
 		if(fd_method == 4)
 		{
 			for(j=0;j<=N;j++)
-				b[N+1+j]         = u[1][j];
-				b[(N-1)*(N+1)+j] = u[N-1][j];
+				b[N+1+j]         = u[N+1+j];
+				b[(N-1)*(N+1)+j] = u[(N-1)*(N+1)+j];
 			for(i=0;i<=N;i++)
-				b[i*(N+1)+1]     = u[i][1];
-				b[i*(N+1)+N-1]   = u[i][N-1];
+				b[i*(N+1)+1]     = u[i*(N+1)+1];
+				b[i*(N+1)+N-1]   = u[i*(N+1)+N-1];
 		}
 	}
 }
