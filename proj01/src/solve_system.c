586630f7bdcd92781263e07d56fdfd6b27d2120d
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-24 22:52:01 -0600

    Update solve_system.c

--- proj01/src/solve_system.c
+++ proj01/src/solve_system.c
@@ -8,7 +8,8 @@ void solve_system(Parameter* solver)
 	/* Variable needed: n, iter_method */
 	int i, j, k;
 	double diag;
-	double n = solver.n;
+	int n = solver.n;
+	double tmp;
 
 	/* Ask for dynamic memory */
 	double* dz;	
@@ -19,55 +20,66 @@ void solve_system(Parameter* solver)
 	if(solver.iter_method == 1)
 	{	
 		for(i=0;i<n;i++)
-			solver.z[i] = solver.b[i];	
-		for(k=0; k<solver.max_iter; k++)
+			solver.z[i] = 0;	
+		for(k=0;k<solver.max_iter;k++)
 		{
-			for(i=0; i<n; i++)
+			for(i=0;i<n;i++)
 			{
-				dz[i] = solver.b[i];
-				for(j=0; j<solver.A.nonzero[i]; j++)
+				tmp = solver.b[i];
+				for(j=0;j<solver.A.nonzero[i];j++)
 				{
 					if(solver.A.col[i][j]!=i)
-						dz[i] -= solver.A.val[i][j] * solver.z[solver.A.col[i][j]];
+						tmp  = tmp - solver.A.val[i][j] * solver.z[solver.A.col[i][j]];
 					else
-						diag   = solver.A.val[i][j];
+						diag = solver.A.val[i][j];
 				}
-				dz[i] = dz[i] / diag; 
+				dz[i] = tmp / diag; 
 			}	
 		
-			if(error_norm(solver.z,dz,n)< solver.eps)
+			if(error_norm(solver.z,dz,n) < solver.eps)
+			{
+				for(i=0;i<n;i++)
+					solver.z[i] = dz[i];
 				break;
+			}
 			else
-				for(i=0; i<n; i++)
+				for(i=0;i<n;i++)
 					solver.z[i] = dz[i];
 		}
+		free(dz);
 	}
 
 	/* Solve linear system using Gauss-Seidel iteration */
-	if(iter_method == 'Gauss_Seidel')
+	if(iter_method == 2)
 	{
-		for(k=0; k<max_iter; k++)
+		for(i=0;i<n;i++)
+			solver.z[i] = 0;
+		for(k=0;k<solver.max_iter;k++)
 		{
-			for(i=0; i<n; i++)
+			for(i=0;i<n;i++)
 			{
-				dz[i] = b[i];
-				for(j=0; j<A.nonzero[i]; j++)
+				tmp = solver.b[i];
+				for(j=0;j<solver.A.nonzero[i];j++)
 				{
-					if(A.col[i][j]<i)
-						dz[i] -= A.val[i][j] * dx[A.col[i][j]];
+					if(solver.A.col[i][j]<i)
+						tmp  = tmp - solver.A.val[i][j] * dz[A.col[i][j]];
 					else if(A.col[i][j]>i)
-						dz[i] -= A.val[i][j] * x[A.col[i][j]];
+						tmp  = tmp - solver.A.val[i][j] * solver.z[A.col[i][j]];
 					else
-						diag   = A.val[i][j];	
+						diag = solver.A.val[i][j];	
 				}
-				dz[i] = dz[i] / diag; 
+				dz[i] = tmp / diag;
 			}	
 		
-			if(error_norm(*z,*dz, n)<= eps) 
+			if(error_norm(solver.z, dz, n) < eps)
+			 	/* If the error_norm is less than eps, then break. */
+				for(i=0;i<n;i++)
+					solver.z[i] = dz[i];
 				break;
-			else  //how to assign value?
-				for(i=0; i<n; i++)
-					z[i] = dz[i];
+			else
+				for(i=0;i<n;i++)
+					solver.z[i] = dz[i];
 		}
+		free(dz);
 	}
 }
