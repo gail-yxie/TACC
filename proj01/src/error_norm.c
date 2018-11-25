733b4bcd03aa81e4f667a54025c52475a182fe64
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-25 12:55:26 -0600

    Update error_norm.c

--- proj01/src/error_norm.c
+++ proj01/src/error_norm.c
@@ -2,14 +2,14 @@
 #include <math.h>
 #include "parameter.h"
 
-double error_norm(double* x, double* y, int n)
+double error_norm(double* x, double* y, int n, int act_n)
 {
 	double error;
 
 	for(int i=0;i<solver.n;i++)
 		error = error + (x[i] - y[i]) * (x[i] - y[i]); 
 	
-	error = sqrt(error/n);
+	error = sqrt(error/act_n);
 	
 	return error;
 }
