f116a03c32c81e8499e7a51bee4ed35f02ffe123
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-24 22:54:24 -0600

    Update function.h

--- proj01/src/function.h
+++ proj01/src/function.h
@@ -6,11 +6,11 @@
 
 /* Function prototypes */
 
-void parse_input(Parameter*solver, const char* input_file);
+void parse_input(Parameter* solver, const char* input_file);
 void init(Parameter* solver);
 void build_linear_system(Parameter* solver);
 void solve_system(Parameter* solver);;
-void output(*Parameter solver);;
-double error_norm(*Parameter solver);
+void output(Parameter* solver);;
+double error_norm(Parameter* solver);
 
 #endif
