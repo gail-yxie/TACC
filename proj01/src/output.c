9ac7c9362501da3ab465f3d4470e8f063af759df
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-24 22:59:08 -0600

    Update output.c

--- proj01/src/output.c
+++ proj01/src/output.c
@@ -1,8 +1,24 @@
-#include <stdio.h
+#include <stdio.h>
+#include <stdlib.h>
+#include "parameter.h"
 
-void output()
+void output(Parameter* solver)
 {
+	/* silent mode */
+	if(solver.output_mode == 0)
+	{
+		//only output the result solver.z;
+	}
 	
-
-
+	/* standard mode */
+	if(solver.output_mode == 1)
+	{
+	
+	}
+	
+	/* debug mode */
+	if(solver.output_mode == 2)
+	{
+	
+	}
 }
