ba4713a22082bd7f381557cc27199eb4290118cc
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-27 15:45:37 -0600

    Update build_linear_system.c

--- proj01/src/build_linear_system.c
+++ proj01/src/build_linear_system.c
@@ -214,7 +214,7 @@ void build_linear_system(struct Parameter* solver)
 		printf("\nb =\n\n[\n");
 		for(i=0;i<solver->n;i++)
 			printf("%f;\n", solver->b[i]);
-		printf("];\n\n")
+		printf("];\n\n");
 	}
 	
 	if(solver->output_mode == 2)
