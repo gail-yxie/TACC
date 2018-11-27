c1909b394f9ad036abbb4c75ad8e41dc7fc6c6b6
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-27 17:01:28 -0600

    Update output.c

--- proj01/src/output.c
+++ proj01/src/output.c
@@ -6,80 +6,36 @@ void output(struct Parameter* solver)
 	
 	int i,j;
 	
+	/* Write solutions of heat equation to file output_file */
 	FILE *fp = NULL;
 	fp = fopen(solver->output_file,"w+");
 	
 	for(i=0;i<solver->n;i++)
 		fprintf(fp, "%.18f ",solver->z[i]);
 	fprintf(fp, "\n");
+	fclose(fp);
 	
+	/* Use l2 norm to verify the solutions*/
 	if(solver->verify_mode == 1)
 	{
-		FILE *fp2;
 		double error;
-		fp2 = fopen("conv_tmp","w+");
 		error = error_norm(solver->z, solver->u, solver->n);
-		fprintf(fp2, "%d %e\n", solver->N ,error);
-		fclose(fp2);
+		
+		printf("Error norm for verification is %e", error);
+		
+		/* If you need to save the error norm in file and to plot convergence rate */
+		\\FILE *fp2;
+		\\fp2 = fopen("conv_tmp","w+");
+		\\fprintf(fp2, "%d %e\n", solver->N ,error);
+		\\fclose(fp2);
 	}
-
-	/* silent mode */
-	if(solver->output_mode == 0)
-	{
-		//only output the result solver->z;
-	}
-
+	
+	/* codes to generate reference data for check, please comment it when using the program. */
 	//FILE *fp1 = NULL;
 	//fp1 = fopen("../test/ref_2d.txt", "w+");
 	//for(i=0;i<solver->n;i++)
                 //fprintf(fp1, "%.18f ",solver->u[i]);
         //fprintf(fp1, "\n"); 
-	
-	/* standard mode */
-	if(solver->output_mode == 3)
-	{
-		/* output the matrix */
-		printf("---matrix---\n");
-
-		for(i=0;i<solver->n;i++)
-                        printf("%d  ", solver->nonzero[i]);
-                printf("\n");
-		for(i=0;i<solver->n;i++)
-		{
-			for(j=0;j<solver->nonzero[i];j++)
-                        	printf("%d  ", solver->col[i][j]);
-                	printf("\n");
-		}
-
-		for(i=0;i<solver->n;i++)
-                {
-                        for(j=0;j<solver->nonzero[i];j++)
-                                printf("%f  ", solver->val[i][j]);
-                        printf("\n");
-                }	
-
-		for(i=0;i<solver->n;i++)
-                        printf("%f  ", solver->z[i]);
-                printf("\n");
-                for(i=0;i<solver->n;i++)
-                        printf("%f  ", solver->u[i]);
-                printf("\n");
-		
-		if(solver->verify_mode == 1)
-		{
-			double error;
-			error = error_norm(solver->z, solver->u, solver->n);
-			printf("Total n is: %d\n Error norm is %e\n", solver->n, error);
-		}	
-	}
-	
-	/* debug mode */
-	if(solver->output_mode == 2)
-	{
-	
-	}
-	
-	fclose(fp);
 
 	 /* Free dynamic variables*/
         free(solver->output_file);
