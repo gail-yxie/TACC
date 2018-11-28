9452180ceb21bb4d1103317b699e3ae5121f81c8
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-11-27 23:01:14 -0600

    Update error_norm.c

--- proj01/src/error_norm.c
+++ proj01/src/error_norm.c
@@ -7,6 +7,8 @@ double error_norm(double* vecx, double* vecy, int vecn)
 	for(int i=0;i<vecn;i++)
 		error = error + (vecx[i] - vecy[i]) * (vecx[i] - vecy[i]); 
 	
+	/* Devide by vecn */
+	/* If you want to calculate exact error, please add an additional act_n to be divided */
 	error = sqrt(error/vecn);
 	
 	return error;
