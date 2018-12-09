f78ed15d5b0b76d6dd700ee8ff8b7e4410d443e4
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-12-09 13:40:49 -0600

    Update output.c

--- proj02/src/output.c
+++ proj02/src/output.c
@@ -10,69 +10,39 @@ void output(struct Parameter* solver)
 	
 	/* Write solutions of heat equation to file output_file self-defined */
 	// need to be comment
-	FILE *fp = NULL;
-	fp = fopen(solver->output_file,"w+");
+	//FILE *fp = NULL;
+	//fp = fopen(solver->output_file,"w+");
 	
-	for(i=0;i<solver->n;i++)
-		fprintf(fp, "%.18f ",solver->z[i]);
-	fprintf(fp, "\n");
-	fclose(fp);
+	//for(i=0;i<solver->n;i++)
+		//fprintf(fp, "%.18f ",solver->z[i]);
+	//fprintf(fp, "\n");
+	//fclose(fp);
 	
 	/* Write solutions of heat equation to file output_file HDF5 */
-	hid_t    file, dataset, dataspace;	/* file and dataset handles */
+	hid_t    file, group, dataset, dataspace;	/* file and dataset handles */
 	hsize_t  dimsf[1];			/* dataset dimensions */
 	herr_t   status;
 	
-	//int 	 ny;
-	//int N = solver->N;
-	
-	//if(solver->dimensions == 1)
-		//ny = 2;
-	//else
-		//ny = 3;
-	//if(solver->verify_mode == 1)
-		//ny = ny + 1;
-	
-	/* Initialize data buffer. */
-	//data = (double**)malloc(sizeof(double*) * solver->n);
-	//for(i=0;i<solver->n;i++)
-		//data[i] = (double*)malloc(sizeof(double) * ny);
-	//double data[17][3];
-	//if(solver->dimensions == 1)
-		//for(i=0;i<=N;i++)
-		//{
-			//data[i][0] = solver->xmin + i * solver->h;
-			//data[i][1] = solver->z[i];
-			//if(solver->verify_mode == 1)
-				//data[i][2] = solver->u[i];
-		//}
-	
-	//if(solver->dimensions == 2)	
-		//for(i=0;i<=N;i++)
-			//for(j=0;j<=N;j++)
-			//{
-				//data[i*(N+1)+j][0] = solver->xmin + i * solver->h;
-				//data[i*(N+1)+j][1] = solver->ymin + j * solver->h;
-				//data[i*(N+1)+j][2] = solver->z[i*(N+1)+j];
-				//if(solver->verify_mode == 1)
-					//data[i*(N+1)+j][3] = solver->u[i*(N+1)+j];
-			//}
-	solver->z[0] = 5;
 	/* Create a new file using default properties. */
-	//need change file name
-	file = H5Fcreate("sol.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	file = H5Fcreate(solver->output_file, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 	/* Create the data space for the dataset. */
 	dimsf[0] = solver->n;
-	//dimsf[1] = ny;
 	dataspace = H5Screate_simple(1, dimsf, NULL);
 	/* Create the dataset using defined dataspace and default properties. */
-	// what does "" mean?
-	dataset = H5Dcreate2(file, "heat eq sol", H5T_NATIVE_DOUBLE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	dataset = H5Dcreate2(file, "cor_x", H5T_NATIVE_DOUBLE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 	/* Write the data to the dataset using default transfer properties. */
-	status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, solver->z);
-	/* Close/release resources. */
+	status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, solver->cor_x);
 	H5Sclose(dataspace);
 	H5Dclose(dataset);
+	
+	dataspace = H5Screate_simple(1, dimsf, NULL);
+	dataset = H5Dcreate2(file, "T_sol", H5T_NATIVE_DOUBLE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, solver->z);
+	H5Sclose(dataspace);
+	H5Dclose(dataset);
+	
+	/* Close/release resources. */
+	
 	H5Fclose(file);
 	
 	if(status!=0)
@@ -105,16 +75,8 @@ void output(struct Parameter* solver)
 		//FILE *fp2;
 		//fp2 = fopen("conv_tmp","w+");
 		//fprintf(fp2, "%d %e\n", solver->N ,error);
-		//fclose(fp2);
-		
+		//fclose(fp2);	
 	}
-	
-	/* codes to generate reference data for check, please comment it when using the program. */
-	//FILE *fp1 = NULL;
-	//fp1 = fopen("../test/ref_2d.txt", "w+");
-	//for(i=0;i<solver->n;i++)
-                //fprintf(fp1, "%.18f ",solver->u[i]);
-        //fprintf(fp1, "\n"); 
 
 	 /* Free dynamic variables*/
         free(solver->output_file);
@@ -125,11 +87,9 @@ void output(struct Parameter* solver)
 	{
 		free(solver->col[i]);
 		free(solver->val[i]);
-		//free(data[i]);
 	}
         free(solver->val);
 	free(solver->col);
-	//free(data);
 	free(solver->f);
         free(solver->u);
 
