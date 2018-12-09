5825cf865ed94226b8bd82ae5e1d2b4e160a855d
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-12-09 13:28:29 -0600

    Update init.c

--- proj02/src/init.c
+++ proj02/src/init.c
@@ -13,7 +13,6 @@ void init(struct Parameter* solver)
 		
 	if(solver->dimensions == 1)
 	{	
-		double x;
 		solver->n = N+1;
 
 		/* Initialize masa: we can choose "bob" or "nick" */
@@ -32,11 +31,11 @@ void init(struct Parameter* solver)
 		for(i=0; i<=N; i++)
 		{      
                         /* generate mesh */
-			x = solver->xmin + i * solver->h;
+			solver->cor_x[i] = solver->xmin + i * solver->h;
 			/* source term */
-			solver->f[i] = masa_eval_1d_source_t(x);
+			solver->f[i] = masa_eval_1d_source_t(solver->cor_x[i]);
 			/* manufactured solution */
-			solver->u[i] = masa_eval_1d_exact_t(x);
+			solver->u[i] = masa_eval_1d_exact_t(solver->cor_x[i]);
 			
 			if((i==0)||(i==N))
 				/* Record boundary conditions*/
@@ -55,7 +54,6 @@ void init(struct Parameter* solver)
 	
 	if(solver->dimensions == 2)
 	{	
-		double x,y;
 		int j;
 		solver->n = (N+1)*(N+1);
 
@@ -77,13 +75,13 @@ void init(struct Parameter* solver)
 			for(j=0; j<=N; j++)
 			{	
 				/* generate mesh */
-				x = solver->xmin + i * solver->h;
-				y = solver->ymin + j * solver->h;
+				solver->cor_x[i*(N+1)+j] = solver->xmin + i * solver->h;
+				solver->cor_y[i*(N+1)+j] = solver->ymin + j * solver->h;
 
 				/* source term */
-				solver->f[i*(N+1)+j] = masa_eval_2d_source_t(x,y);
+				solver->f[i*(N+1)+j] = masa_eval_2d_source_t(solver->cor_x[i*(N+1)+j],solver->cor_y[i*(N+1)+j]);
 				/* manufactured solution */
-				solver->u[i*(N+1)+j] = masa_eval_2d_exact_t(x,y);
+				solver->u[i*(N+1)+j] = masa_eval_2d_exact_t(solver->cor_x[i*(N+1)+j],solver->cor_y[i*(N+1)+j]);
 		
 				if((i==0)||(i==N)||(j==0)||(j==N))
 					/* Record boundary conditions*/
