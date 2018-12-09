7396d1db1829091df285aa5cdd13047f72585378
Yuege Xie <yx1023@login2.stampede2.tacc.utexas.edu>
2018-12-09 12:36:37 -0600

    update the method to write to hdf5

--- proj02/src/output.c
+++ proj02/src/output.c
@@ -20,57 +20,56 @@ void output(struct Parameter* solver)
 	
 	/* Write solutions of heat equation to file output_file HDF5 */
 	hid_t    file, dataset, dataspace;	/* file and dataset handles */
-	hsize_t  dimsf[2];			/* dataset dimensions */
+	hsize_t  dimsf[1];			/* dataset dimensions */
 	herr_t   status;
 	
-	int 	 ny;
-	//double** data;
-	int N = solver->N;
+	//int 	 ny;
+	//int N = solver->N;
 	
-	if(solver->dimensions == 1)
-		ny = 2;
-	else
-		ny = 3;
-	if(solver->verify_mode == 1)
-		ny = ny + 1;
+	//if(solver->dimensions == 1)
+		//ny = 2;
+	//else
+		//ny = 3;
+	//if(solver->verify_mode == 1)
+		//ny = ny + 1;
 	
 	/* Initialize data buffer. */
 	//data = (double**)malloc(sizeof(double*) * solver->n);
 	//for(i=0;i<solver->n;i++)
 		//data[i] = (double*)malloc(sizeof(double) * ny);
-	double data[17][3];
-	if(solver->dimensions == 1)
-		for(i=0;i<=N;i++)
-		{
-			data[i][0] = solver->xmin + i * solver->h;
-			data[i][1] = solver->z[i];
-			if(solver->verify_mode == 1)
-				data[i][2] = solver->u[i];
-		}
+	//double data[17][3];
+	//if(solver->dimensions == 1)
+		//for(i=0;i<=N;i++)
+		//{
+			//data[i][0] = solver->xmin + i * solver->h;
+			//data[i][1] = solver->z[i];
+			//if(solver->verify_mode == 1)
+				//data[i][2] = solver->u[i];
+		//}
 	
-	if(solver->dimensions == 2)	
-		for(i=0;i<=N;i++)
-			for(j=0;j<=N;j++)
-			{
-				data[i*(N+1)+j][0] = solver->xmin + i * solver->h;
-				data[i*(N+1)+j][1] = solver->ymin + j * solver->h;
-				data[i*(N+1)+j][2] = solver->z[i*(N+1)+j];
-				if(solver->verify_mode == 1)
-					data[i*(N+1)+j][3] = solver->u[i*(N+1)+j];
-			}
-
+	//if(solver->dimensions == 2)	
+		//for(i=0;i<=N;i++)
+			//for(j=0;j<=N;j++)
+			//{
+				//data[i*(N+1)+j][0] = solver->xmin + i * solver->h;
+				//data[i*(N+1)+j][1] = solver->ymin + j * solver->h;
+				//data[i*(N+1)+j][2] = solver->z[i*(N+1)+j];
+				//if(solver->verify_mode == 1)
+					//data[i*(N+1)+j][3] = solver->u[i*(N+1)+j];
+			//}
+	solver->z[0] = 5;
 	/* Create a new file using default properties. */
 	//need change file name
-	file = H5Fcreate("try.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	file = H5Fcreate("sol.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 	/* Create the data space for the dataset. */
 	dimsf[0] = solver->n;
-	dimsf[1] = ny;
-	dataspace = H5Screate_simple(2, dimsf, NULL);
+	//dimsf[1] = ny;
+	dataspace = H5Screate_simple(1, dimsf, NULL);
 	/* Create the dataset using defined dataspace and default properties. */
 	// what does "" mean?
 	dataset = H5Dcreate2(file, "heat eq sol", H5T_NATIVE_DOUBLE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 	/* Write the data to the dataset using default transfer properties. */
-	status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+	status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, solver->z);
 	/* Close/release resources. */
 	H5Sclose(dataspace);
 	H5Dclose(dataset);
