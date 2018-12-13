bdf858697f614e78017acf744b1eff7e7cd048c0
willway1023yx <36209805+willway1023yx@users.noreply.github.com>
2018-12-12 21:38:39 -0600

    Update solve_system.c

--- proj02/src/solve_system.c
+++ proj02/src/solve_system.c
@@ -7,87 +7,8 @@ void solve_system(struct Parameter* solver)
 	if(solver->output_mode !=0)
 		printf("\n\n** Solving linear system...\n");
 	
-	if(solver->iter_method!=3)
-	{
-		int i, j;
-		int k=0;
-		double diag = 1;
-		int n = solver->n;
-		double tmp;
-
-		/* Ask for dynamic memory */
-		double* old_z;	
-		old_z = (double*)malloc(n*sizeof(double));
-		solver->z  = (double*)malloc(n*sizeof(double));
-	}
-	/* Solve linear system using Jacobi iteration */
-	if(solver->iter_method == 1)
-	{	
-		for(i=0;i<n;i++)
-			old_z[i] = 0;	
-		for(k=0;k<solver->max_iter;k++)
-		{
-			for(i=0;i<n;i++)
-			{
-				tmp = solver->b[i];
-				for(j=0;j<solver->nonzero[i];j++)
-				{
-					if(solver->col[i][j]!=i)
-						tmp  = tmp - solver->val[i][j] * old_z[solver->col[i][j]];
-					else
-						diag = solver->val[i][j];
-				}
-				solver->z[i] = tmp / diag; 
-			}
-			/* Output tmp error */
-			double tmp_error = error_norm(solver->z, old_z, n);
-			if(solver->output_mode ==2 )
-				printf("   [ current convergence eps = %e ]\n", tmp_error);
-			if(tmp_error < solver->eps)
-				break;
-			else
-				for(i=0;i<n;i++)
-					old_z[i] = solver->z[i];
-		}
-		free(old_z);
-	}
-
-	/* Solve linear system using Gauss-Seidel iteration */
-	else if(solver->iter_method == 2)
-	{
-		for(i=0;i<n;i++)
-		{
-			solver->z[i] = 0;
-			old_z[i]     = 0;
-		}
-		for(k=0;k<solver->max_iter;k++)
-		{
-			for(i=0;i<n;i++)
-			{
-				tmp = solver->b[i];
-				for(j=0;j<solver->nonzero[i];j++)
-				{
-					if(solver->col[i][j]!=i)
-						tmp  = tmp - solver->val[i][j] * solver->z[solver->col[i][j]];
-					else
-						diag = solver->val[i][j];	
-				}
-				solver->z[i] = tmp / diag;
-			}	
-			double tmp_error = error_norm(solver->z, old_z, n);
-			if(solver->output_mode ==2 )
-				printf("   [ current convergence eps = %e ]\n", tmp_error);
-			if( tmp_error < solver->eps)
-				break;
-			else
-				for(i=0;i<n;i++)
-					old_z[i] = solver->z[i];
-		}
-		free(old_z);
-	}
-	
 	#ifdef HAVE_PETSC
-	else if(solver->iter_method == 3)
+	if(solver->iter_method == 3)
 	{
 		
 		//write PETSC code
@@ -157,6 +78,88 @@ void solve_system(struct Parameter* solver)
 			
 	}
 	#endif
+	
+	else
+	{
+		int i, j;
+		int k=0;
+		double diag = 1;
+		int n = solver->n;
+		double tmp;
+
+		/* Ask for dynamic memory */
+		double* old_z;	
+		old_z = (double*)malloc(n*sizeof(double));
+		solver->z  = (double*)malloc(n*sizeof(double));
+	}
+	
+	/* Solve linear system using Jacobi iteration */
+	if(solver->iter_method == 1)
+	{	
+		for(i=0;i<n;i++)
+			old_z[i] = 0;	
+		for(k=0;k<solver->max_iter;k++)
+		{
+			for(i=0;i<n;i++)
+			{
+				tmp = solver->b[i];
+				for(j=0;j<solver->nonzero[i];j++)
+				{
+					if(solver->col[i][j]!=i)
+						tmp  = tmp - solver->val[i][j] * old_z[solver->col[i][j]];
+					else
+						diag = solver->val[i][j];
+				}
+				solver->z[i] = tmp / diag; 
+			}
+			/* Output tmp error */
+			double tmp_error = error_norm(solver->z, old_z, n);
+			if(solver->output_mode ==2 )
+				printf("   [ current convergence eps = %e ]\n", tmp_error);
+			if(tmp_error < solver->eps)
+				break;
+			else
+				for(i=0;i<n;i++)
+					old_z[i] = solver->z[i];
+		}
+		free(old_z);
+	}
+
+	/* Solve linear system using Gauss-Seidel iteration */
+	else if(solver->iter_method == 2)
+	{
+		for(i=0;i<n;i++)
+		{
+			solver->z[i] = 0;
+			old_z[i]     = 0;
+		}
+		for(k=0;k<solver->max_iter;k++)
+		{
+			for(i=0;i<n;i++)
+			{
+				tmp = solver->b[i];
+				for(j=0;j<solver->nonzero[i];j++)
+				{
+					if(solver->col[i][j]!=i)
+						tmp  = tmp - solver->val[i][j] * solver->z[solver->col[i][j]];
+					else
+						diag = solver->val[i][j];	
+				}
+				solver->z[i] = tmp / diag;
+			}	
+			double tmp_error = error_norm(solver->z, old_z, n);
+			if(solver->output_mode ==2 )
+				printf("   [ current convergence eps = %e ]\n", tmp_error);
+			if( tmp_error < solver->eps)
+				break;
+			else
+				for(i=0;i<n;i++)
+					old_z[i] = solver->z[i];
+		}
+		free(old_z);
+	}
+	
+	
 	
 	/* Print number of iterations */
 	if(solver->output_mode != 0)
