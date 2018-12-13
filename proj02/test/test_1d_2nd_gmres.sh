6def8eaa2184cabe817ae8ffee758cf93ca8c949
Yuege Xie <yx1023@login3.stampede2.tacc.utexas.edu>
2018-12-13 16:54:39 -0600

    update tests

---
+++ proj02/test/test_1d_2nd_gmres.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+executable="../src/solver"
+
+# verify executable exists
+if [ ! -x "$executable" ]; then
+	echo "Error: expecting executable -> $executable"
+	exit 1
+fi
+
+# verify 1d 2nd gmres
+mpirun -np 1 $executable input.1d.2nd.gmres
+h5diff --relative=10 ref_1d_2nd_gmres.h5 sol.dat T_sol T_sol
+if [ $(echo $?) -ne 0 ]; then
+	echo "Error: error norm is too large!"
+	exit 1
+fi
---
+++ proj02/test/test_1d_4th_gauss.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+executable="../src/solver"
+
+# verify executable exists
+if [ ! -x "$executable" ]; then
+	echo "Error: expecting executable -> $executable"
+	exit 1
+fi
+
+# verify 1d 4th gauss
+$executable input.1d.4th.gauss
+h5diff --use-system-epsilon ref_1d_4th_gauss.h5 sol.dat T_sol T_sol
+if [ $(echo $?) -ne 0 ]; then
+	echo "Error: error norm is too large!"
+	exit 1
+fi
